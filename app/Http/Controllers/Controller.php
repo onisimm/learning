2679dea2b149e332bc1ca127a7f1f36567b8b487
onisimm <onisim_roman@yahoo.com>
2022-11-22 20:59:36 +0200

    Initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function test() {
+        $user = new User();
+        $user->firstname = 'Popescu';
+        $user->lastname = "Mircea";
+    }
+}
