1790798120033eb5df6e584aa9e4922756c52dcd
Onisim Roman <84021899+onisimm@users.noreply.github.com>
2022-12-05 21:41:38 +0200

    added allVideos page & specific video page
    + small visual updates

---
+++ resources/views/allVideos.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>All videos</title>
+
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/general.css?v=').time()}}" >
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/allVideos.css?v=').time()}}" >
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/latestVideos.css?v=').time()}}" >
+    </head>
+    <body>
+        <div class="center div-box">
+            <h1>Videos</h1>
+            <p>Watch videos on biblical topics and on many areas of practical Christian living, on over 40 languages. Enjoy!</p>
+            
+            <div class="flexColumnContainer">
+                <div class="flexRowContainer inColumn">
+                    <div class="videoThumbnail">
+                        <a href="#"><img src="{{ asset('images/thumbnail.jpg') }}" alt="Video thumbnail" class="latestVideoThumbnail"></a>
+                    </div>
+                    <div class="videoTitleDescription">
+                        <div class="flexColumnContainer">
+                            <a href="#"><div class="videoTitle"><h3>Titlul video-ului</h3></div></a>
+                            <div class="videoReleaseDate">Data - Autor</div>
+                            <div class="videoDescription">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias voluptates illum ipsum commodi enim veritatis sunt nisi, dolorem in, inventore sequi maxime! Veritatis maxime neque consequatur optio obcaecati soluta excepturi rerum quia tenetur ab quidem error possimus fugiat nobis sequi delectus, amet quod assumenda, porro dicta totam omnis? Dolorum quasi est magni impedit minus perspiciatis ipsa, dolores aliquid ipsam delectus doloremque, reprehenderit, odit error culpa eveniet consectetur veritatis. Maiores voluptatum modi facilis laborum harum esse illo aspernatur in ab quod eius atque, a minus accusamus repellat autem suscipit excepturi tempore eveniet non. Laborum consectetur dolorem maiores dolor, qui magnam aut?
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flexRowContainer inColumn">
+                    <div class="videoThumbnail">
+                        <a href="#  "><img src="{{ asset('images/thumbnail.jpg') }}" alt="Video thumbnail" class="latestVideoThumbnail"></a>
+                    </div>
+                    <div class="videoTitleDescription">
+                        <div class="flexColumnContainer">
+                            <a href="#"><div class="videoTitle"><h3>Titlul video-ului</h3></div></a>
+                            <div class="videoReleaseDate">Data - Autor</div>
+                            <div class="videoDescription">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias voluptates illum ipsum commodi enim veritatis sunt nisi, dolorem in, inventore sequi maxime! Veritatis maxime neque consequatur optio obcaecati soluta excepturi rerum quia tenetur ab quidem error possimus fugiat nobis sequi delectus, amet quod assumenda, porro dicta totam omnis? Dolorum quasi est magni impedit minus perspiciatis ipsa, dolores aliquid ipsam delectus doloremque, reprehenderit, odit error culpa eveniet consectetur veritatis. Maiores voluptatum modi facilis laborum harum esse illo aspernatur in ab quod eius atque, a minus accusamus repellat autem suscipit excepturi tempore eveniet non. Laborum consectetur dolorem maiores dolor, qui magnam aut?
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flexRowContainer inColumn">
+                    <div class="videoThumbnail">
+                        <a href="#"><img src="{{ asset('images/thumbnail.jpg') }}" alt="Video thumbnail" class="latestVideoThumbnail"></a>
+                    </div>
+                    <div class="videoTitleDescription">
+                        <div class="flexColumnContainer">
+                            <a href="#"><div class="videoTitle"><h3>Titlul video-ului</h3></div></a>
+                            <div class="videoReleaseDate">Data - Autor</div>
+                            <div class="videoDescription">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias voluptates illum ipsum commodi enim veritatis sunt nisi, dolorem in, inventore sequi maxime! Veritatis maxime neque consequatur optio obcaecati soluta excepturi rerum quia tenetur ab quidem error possimus fugiat nobis sequi delectus, amet quod assumenda, porro dicta totam omnis? Dolorum quasi est magni impedit minus perspiciatis ipsa, dolores aliquid ipsam delectus doloremque, reprehenderit, odit error culpa eveniet consectetur veritatis. Maiores voluptatum modi facilis laborum harum esse illo aspernatur in ab quod eius atque, a minus accusamus repellat autem suscipit excepturi tempore eveniet non. Laborum consectetur dolorem maiores dolor, qui magnam aut?
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="flexRowContainer inColumn">
+                    <div class="videoThumbnail">
+                        <a href="#"><img src="{{ asset('images/thumbnail.jpg') }}" alt="Video thumbnail" class="latestVideoThumbnail"></a>
+                    </div>
+                    <div class="videoTitleDescription">
+                        <div class="flexColumnContainer">
+                            <a href="#"><div class="videoTitle"><h3>Titlul video-ului</h3></div></a>
+                            <div class="videoReleaseDate">Data - Autor</div>
+                            <div class="videoDescription">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias voluptates illum ipsum commodi enim veritatis sunt nisi, dolorem in, inventore sequi maxime! Veritatis maxime neque consequatur optio obcaecati soluta excepturi rerum quia tenetur ab quidem error possimus fugiat nobis sequi delectus, amet quod assumenda, porro dicta totam omnis? Dolorum quasi est magni impedit minus perspiciatis ipsa, dolores aliquid ipsam delectus doloremque, reprehenderit, odit error culpa eveniet consectetur veritatis. Maiores voluptatum modi facilis laborum harum esse illo aspernatur in ab quod eius atque, a minus accusamus repellat autem suscipit excepturi tempore eveniet non. Laborum consectetur dolorem maiores dolor, qui magnam aut?
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/video.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Video</title>
+
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/general.css?v=').time()}}" >
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/latestVideos.css?v=').time()}}" >
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/video.css?v=').time()}}" >
+    </head>
+    <body>
+        <div class="center videoContainer">
+            <iframe src="https://www.youtube.com/embed/kgrV3_g9rYY" allow="fullscreen;"></iframe>
+        </div>
+        <div class="center div-box">
+            <h1>Video Title</h1>
+            <div class="categoryLabel">Categorie</div>
+            <div class="videoReleaseDate">Data - Autor</div>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint optio quasi, odio sapiente quibusdam tempora quia eveniet molestiae, porro nisi obcaecati ipsa numquam facere iure deserunt exercitationem sunt harum quo quos unde id necessitatibus veniam cum! A molestiae inventore ipsa unde eum maiores ad! Dicta veniam quam alias. Ab, molestias cum ducimus tenetur laboriosam provident suscipit qui assumenda dolorem sed voluptas reiciendis, quisquam nulla voluptatum magnam debitis odio distinctio magni quis. Dolorum ea veritatis eum quod nihil, mollitia reprehenderit quasi modi, recusandae necessitatibus distinctio quis pariatur blanditiis, expedita voluptate porro? Laboriosam repudiandae deleniti suscipit laudantium velit at sed explicabo esse?</p>
+            </div>
+        </div>
+    </body>
+</html>
